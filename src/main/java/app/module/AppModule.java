17b3f41d2a4b4d81dfdfeb3e0f2a12e0d7961715
kito99 <kito.mann@virtua.com>
2016-09-15 06:33:02 -0400

    Fixed build by adding a package-info with @Root annotation. Also moved Button import to the proper location.

--- src/main/java/app/module/AppModule.java
+++ src/main/java/app/module/AppModule.java
@@ -3,9 +3,10 @@ package app.module;
 import app.component.AppComponent;
 import def.angular.core.NgModule;
 import def.angular.platform_browser.BrowserModule;
+import def.primeng.primeng.Button;
 
 @NgModule(
-	  imports = { BrowserModule.class },
+	  imports = { BrowserModule.class, Button.class },
 	  declarations = { AppComponent.class },
 	  bootstrap = { AppComponent.class }
 	)
---
+++ src/main/java/def/primeng/package-info.java
@@ -0,0 +1,3 @@
+/** This package contains the def.primeng library (translated by www.jsweet.org). */
+@jsweet.lang.Root
+package def.primeng;
